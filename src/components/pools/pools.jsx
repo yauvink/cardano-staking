1f4d53d4b4fa75f9de78bf92adb5c02cd6d7569b
Yauhen <yau.vink@gmail.com>
2022-06-08 20:54:32 +0200

    Add staking pools

--- src/components/pools/pools.jsx
+++ src/components/pools/pools.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Pool from '../pool';
-import styles from './pools.module.css'
+import connectBtnIcon from '../../assets/connectBtnIcon.svg';
+import disconnectBtnIcon from '../../assets/disconnectBtnIcon.svg';
+import styles from './pools.module.css';
 
 export default function Pools({ walletAddress, connected, addresses, nami, setStakeAddress, setOpen, connect }) {
+  const [walletBalance, setWalletBalance] = useState();
+
+  useEffect(() => {
+    if (nami && walletAddress) {
+      nami.getAddressAmount(walletAddress).then((result) => {
+        setWalletBalance(result.amount[0].quantity);
+      });
+    }
+  }, [nami, walletAddress]);
+
   return (
     <div className={styles.wrapper}>
-      <button className="connectButton" onClick={connect}>
-        {connected && walletAddress
-          ? `${walletAddress.substring(0, 6)}....${walletAddress.substring(walletAddress.length - 6)}`
-          : 'Connect wallet'}
-      </button>
+      <div className={styles.header}>
+        <span className={styles.title}>Staking pools</span>
+        {connected && walletAddress ? (
+          <div className={styles.walletInfoWrapper}>
+            <div className={styles.walletInfo}>
+              <span>Available balance:</span>
+              <span>{walletBalance / 1000000} ADAL</span>
+            </div>
+            <div className={styles.walletInfo}>
+              <span>Wallet address:</span>
+              <span>{`${walletAddress.substring(0, 4)}....${walletAddress.substring(walletAddress.length - 4)}`}</span>
+            </div>
+            <button
+              className={styles.disconnectBtn}
+              onClick={() => {
+                console.log('disconnect wallet event');
+              }}
+            >
+              <span>Disconnect</span>
+              <img src={disconnectBtnIcon} alt="icon"></img>
+            </button>
+          </div>
+        ) : (
+          <button className={styles.connectBtn} onClick={connect}>
+            <img src={connectBtnIcon} alt="icon"></img>
+            <span>Connect wallet</span>
+          </button>
+        )}
+      </div>
       {connected &&
-        nami &&
+        nami ?
         addresses.map((stakeAddress, index) => (
           <Pool
             key={index}
@@ -21,7 +57,7 @@ export default function Pools({ walletAddress, connected, addresses, nami, setSt
             setStakeAddress={setStakeAddress}
             setOpen={setOpen}
           ></Pool>
-        ))}
+        )) : 'Not available'}
     </div>
   );
 }
